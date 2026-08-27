b4f465c4e9947f08d3cc9e05f46a6b1567139076
umershaikh123 <umershaikh217@gmail.com>
2026-08-27 14:26:25 +0500

    chore : remove react-loading-spinner

--- src/shared/components/ui/loading.tsx
+++ src/shared/components/ui/loading.tsx
@@ -1,20 +1,16 @@
 "use client"
 
-import { ThreeDots, Puff } from "react-loader-spinner"
 import { Spinner } from "@/shared/components/ui/spinner"
-
+import { cva } from "class-variance-authority"
+import { cn } from "@/shared/lib/utils"
 type LoadingProps = {
   color?: string
   height?: string | number
   width?: string | number
 }
 
-function Loading({
-  color = "#4fa94d",
-  height = "80",
-  width = "80",
-}: LoadingProps) {
-  return <Spinner className="size-10" />
+function Loading({ color = "#4fa94d" }: LoadingProps) {
+  return <Spinner className={cn("size-10", color)} />
 }
 
 export { Loading }
